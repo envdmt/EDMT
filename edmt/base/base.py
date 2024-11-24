5b505941ac07e66a0ac1ef249f0490950139489e
oderofrancis <francisodero10@gmail.com>
2024-11-25 01:07:38 +0300

    EDMT - Generation of the setup : 5

--- edmt/base/base.py
+++ edmt/base/base.py
@@ -1,1 +1,14 @@
 from ..conversion.gen_uuid import *
+from . import  version
+
+# check version
+
+"""
+Example 
+
+import edmt
+edmt.__version__
+
+"""
+
+__version__ = version.get_versions()["version"]
