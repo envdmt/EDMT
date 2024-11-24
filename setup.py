46e47c0d6d00d1f58244d73fcecb890434892723
oderofrancis <francisodero10@gmail.com>
2024-11-25 01:00:47 +0300

    EDMT - Generation of the setup : 3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='edmt',  # Replace with your package name
-    version='1.0.0.1',          # Initial version
+    version='1.0.0.2',          # Initial version
     author='Odero & Kuloba',
     author_email='francisodero@maraelephantproject.org',
     description='Environmental Data Management Toolbox',
