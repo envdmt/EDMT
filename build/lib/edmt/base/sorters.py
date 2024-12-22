0a8fd3cb48803f83590676f635dee7dccb714af2
oderofrancis <francisodero10@gmail.com>
2024-12-22 13:48:39 +0300

    EDMT  - Base : Reconstructing for requirements for EDMT : 01

--- build/lib/edmt/base/sorters.py
+++ build/lib/edmt/base/sorters.py
@@ -1,3 +1,24 @@
+# use sklaern to add datasets and sorters for filling Nan Values by impute
+import geopandas as gpd
+import pandas as pd
+
 def sorters(df=None):
     print("Its working")
+    return df
+
+def datasets(geom=None):
+
+    df = pd.read_csv() # use this for plotting and remove geometry columns
+
+    if geom:
+        df = gpd.GeoDataFrame(
+            df, geom='geonetry'
+        ) # convert the dataset to a geodataframe
+    else:
+        return df
+
+def filler(df,column=None,method=None):
+    # remove nan values by filling using Sklearn impute
+    if column:
+        print(f"{column} filled {method} filler")
     return df
