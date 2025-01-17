985455a97b1b9f5487d038df6f597dbb0e7b0947
oderofrancis <francisodero10@gmail.com>
2025-01-17 23:40:28 +0300

    EDMT - V0.0.1 Merge : 04 - Test

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='edmt',  # Replace with your package name
-    version='1.0.0.1',          # Initial version
+    name='edmt',
+    version='1.0.0.1',        
     author='Odero & Kuloba',
     author_email='francisodero@maraelephantproject.org',
     description='Environmental Data Management Toolbox',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/envdmt/EDMT.git',  # Replace with your repo URL
-    packages=find_packages(),  # Automatically find packages in your repo
+    url='https://github.com/envdmt/EDMT.git',
+    packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',  # Adjust as needed
+        'License :: OSI Approved :: MIT License', 
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',  # Specify the Python versions supported
+    python_requires='>=3.6',  
     install_requires=[
         'geopandas==0.14.2',
         'plotly==5.24.1',
@@ -27,30 +27,3 @@ setup(
         'fiona==1.9.6'
     ],
 )
-# from setuptools import setup, find_packages
-
-# setup(
-#     name='edmt',  # Replace with your package name
-#     version='1.0.0.2',          # Initial version
-#     author='Odero & Kuloba',
-#     author_email='francisodero@maraelephantproject.org',
-#     description='Environmental Data Management Toolbox',
-#     long_description=open('README.md').read(),
-#     long_description_content_type='text/markdown',
-#     url='https://github.com/envdmt/EDMT.git',  # Replace with your repo URL
-#     packages=find_packages(),  # Automatically find packages in your repo
-#     classifiers=[
-#         'Programming Language :: Python :: 3',
-#         'License :: OSI Approved :: MIT License',  # Adjust as needed
-#         'Operating System :: OS Independent',
-#     ],
-#     python_requires='>=3.6',  # Specify the Python versions supported
-#     install_requires=[
-#         'geopandas==1.0.1',
-#         'plotly==5.24.1',
-#         'tqdm==4.66.5',
-#         'folium==0.18.0',
-#         'mapclassify==2.8.1',
-#         'matplotlib==3.9.2'
-#     ],
-# )
