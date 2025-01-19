abfb10d0d73a82c5773ba047c43b44ccf5c522a9
oderofrancis <francisodero10@gmail.com>
2025-01-19 12:20:07 +0300

    EDMT : Mapping-mapping  : Mapping() gplot() - Sorting an Issue in Building the Package & Changed Python Version to Python >=3.10.12 : 01

--- build/lib/edmt/mapping/mapping.py
+++ build/lib/edmt/mapping/mapping.py
@@ -5,12 +5,9 @@ import geopandas as gpd
 import pandas as pd
 import matplotlib.pyplot as plt
 import contextily as cx
+from pyproj import CRS
 
 from edmt.contrib.utils import clean_vars
-
-import time
-from datetime import datetime
-
 
 
 class Mapping:
@@ -20,59 +17,50 @@ class Mapping:
         pass
 
     @staticmethod
-    def gplot(df, column=None, title=None, ax=None, legend=True, fill=None,grids=None):#, **additional_args):
-        # print(f"Plot started at: {datetime.now()}")
-        # start_time = time.time()
-
+    def gplot(df, column=None, title=None, ax=None, legend=True, fill=None, grids=None):
         df = df.copy()
-        df = df.to_crs(epsg=4326)  # Ensure WGS 84
-
-        # Create plot
+        if df.crs is None:
+            raise ValueError("Input GeoDataFrame must have a CRS defined.")
+        if df.crs != CRS.from_epsg(4326):
+            df = df.to_crs(epsg=4326)
+        
         if ax is None:
             _, ax = plt.subplots(figsize=(10, 10))
 
-        # Default plot arguments
         plot_args = {
             "ax": ax,
             "alpha": 0.6,
             "edgecolor": "black",
-            "column": column,
-            "legend": legend,
-            "legend_kwds": {
+        }
+
+        if column:
+            plot_args["column"] = column
+            plot_args["legend"] = legend
+            plot_args["legend_kwds"] = {
                 "loc": "lower right",
                 "bbox_to_anchor": (1, 0),
                 "frameon": True,
                 "title": column,
-            },
-            "facecolor" : fill
-        }
+            }
 
-        # Title
+        if fill:
+            plot_args["color"] = fill
+
         if title:
             ax.set_title(title, fontsize=14)
 
-        # Add grids
         if grids:
             ax.grid(visible=True, linestyle="--", linewidth=0.5, alpha=0.7)
 
-        # Clean and merge additional arguments
-        # plot_args = clean_vars(additional_args, **plot_args)
-
-        # Plot the GeoDataFrame
         df.plot(**plot_args)
 
-        # Add a frame around the map
         for spine in ax.spines.values():
             spine.set_edgecolor("black")
             spine.set_linewidth(1.5)
 
-        # Add basemap
         cx.add_basemap(ax, crs=df.crs, source=cx.providers.OpenStreetMap.Mapnik)
-
-        # end_time = time.time()
-        # execution_time = end_time - start_time
-        # print(f"Execution time: {execution_time:.2f} seconds.")
         return ax
+
 
     
     @staticmethod
