79d5be53df44502e709747e60d26300e6ad35b22
oderofrancis <francisodero10@gmail.com>
2025-02-12 22:01:42 +0300

    EDMT - Conversion : length_conversion(),convert_time() : 01

--- edmt/conversion/__init__.py
+++ edmt/conversion/__init__.py
@@ -5,4 +5,19 @@ from .conversion import (
     read_file_from_url
 )
 
-__all__ = ['sdf_to_gdf', 'generate_uuid', 'get_utm_epsg','read_file_from_url']
+from .checks import (
+   length_conversion
+)
+
+from .time import (
+    convert_time
+)
+
+__all__ = [
+    'sdf_to_gdf', 
+    'generate_uuid', 
+    'get_utm_epsg',
+    'read_file_from_url',
+    'length_conversion',
+    'convert_time'
+    ]
