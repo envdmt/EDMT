e644df31515f3d9a5c670bc8623c4f00c556a349
oderofrancis <francisodero10@gmail.com>
2025-02-13 23:14:20 +0300

    EDMT : Mapping - Mapping() : Testing: 01

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ versions = [
 
 setup(
     name='edmt',
-    version=versions[2],   
+    version=versions[0],   
     author='Odero, Kuloba & musasia',
     author_email='francisodero10@gmail.com',
     description='Environmental Data Management Toolbox',
