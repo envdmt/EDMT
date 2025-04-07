97813c05e53d05d662ffee2442d85a9ae52324d1
oderofrancis <francisodero10@gmail.com>
2025-04-07 23:03:08 +0300

    EDMT : Conversion - Pickle to Geopackage : Status - HEA+ : 02

--- edmt/conversion/__init__.py
+++ edmt/conversion/__init__.py
@@ -2,7 +2,6 @@ from .conversion import (
     sdf_to_gdf,
     generate_uuid,
     get_utm_epsg,
-    read_file_from_url,
     length_conversion,
     convert_time,
     convert_speed
@@ -12,7 +11,6 @@ __all__ = [
     'sdf_to_gdf', 
     'generate_uuid', 
     'get_utm_epsg',
-    'read_file_from_url',
     'length_conversion',
     'convert_time',
     'convert_speed'
