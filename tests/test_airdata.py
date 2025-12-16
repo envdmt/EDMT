f75664280bd0914f8523a4202471e039eaded607
oderofrancis <francisodero10@gmail.com>
2025-12-16 22:36:36 +0300

    EDMT : Models - Drones : optimize CSV ingestion pipeline with session reuse and batched concat : 03

--- tests/test_airdata.py
+++ tests/test_airdata.py
@@ -1,13 +1,2 @@
-import edmt
-from edmt.conversion import to_gdf
-
-# print(f"EDMT Version: {edmt.__version__}")
-# # print all funtions in Airdata
-# airdata_functions = [func for func in dir(Airdata) if callable(getattr(Airdata, func)) and not func.startswith("__")]
-# print("Airdata Functions:")
-# for func in airdata_functions:
-#     print(f"- {func}")
-
-
 def test_airdata():
     assert 1 + 1 == 2
