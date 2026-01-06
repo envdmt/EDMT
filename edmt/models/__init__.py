ee34fb30cf06aa97a6e15d25b5f8278d44cd4d47
oderofrancis <francisodero10@gmail.com>
2026-01-06 22:33:28 +0300

    EDMT : Airdata : get_flight_route : New feature : 03

--- edmt/models/__init__.py
+++ edmt/models/__init__.py
@@ -1,19 +1,11 @@
 from .drones import (
     Airdata,
-    airPoint,
-    df_to_gdf,
-    airLine,
-    airSegment
+    get_flight_routes
 )
 
-from .get_flight_routes import get_flight_routes
 
 __all__ = [
     "Airtable",
     "Airdata",
-    "airPoint",
-    "df_to_gdf",
-    "airLine",
-    "airSegment",
     "get_flight_routes",
 ]
