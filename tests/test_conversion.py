5bc411d267023d353d575a7f749d4ad3bdd7f9d1
oderofrancis <francisodero10@gmail.com>
2026-01-31 09:56:20 +0300

    EDMT : Conversion - Updated Generate UUID & Deprecate df_to_gdf : 08

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -14,27 +14,6 @@ from edmt.conversion import (
 )
 
 # --------------------------
-# Test: convert_time
-# --------------------------
-def test_convert_time():
-    assert convert_time(60, "seconds", "minutes") == 1.0
-    assert convert_time(1, "hour", "seconds") == 3600.0
-    assert convert_time(1000, "ms", "s") == 1.0
-    assert convert_time(1, "microsecond", "s") == 0.0
-    assert convert_time(2629800, "seconds", "month") == 1.0
-
-def test_convert_time_invalid_unit():
-    with pytest.raises(ValueError, match="Invalid 'unit_from'"):
-        convert_time(10, "blargh", "seconds")
-    with pytest.raises(ValueError, match="Invalid 'unit_to'"):
-        convert_time(10, "seconds", "xyz")
-
-def test_convert_time_negative_value():
-    with pytest.raises(ValueError, match="'time_value' must be a non-negative number"):
-        convert_time(-5, "s", "min")
-
-
-# --------------------------
 # Test: convert_speed
 # --------------------------
 def test_convert_speed():
