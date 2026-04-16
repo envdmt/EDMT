599187445500b64c84f6d17dc57d4200f95f5c7f
oderofrancis <francisodero10@gmail.com>
2026-04-16 16:42:50 +0300

    EDMT : Testing

--- edmt/conversion/__init__.py
+++ edmt/conversion/__init__.py
@@ -10,6 +10,10 @@ from .conversion import (
     format_temperature
 )
 
+from .test_conversion import (
+    print_name
+)
+
 __all__ = [
     'sdf_to_gdf', 
     'generate_uuid',
@@ -19,5 +23,6 @@ __all__ = [
     'convert_speed',
     'convert_distance',
     'convert_temperature',
-    'format_temperature'
+    'format_temperature',
+    'print_name'
     ]
